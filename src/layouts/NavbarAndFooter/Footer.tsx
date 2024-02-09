61bfcde61728f5dca033546cba9d13fc00316b64
Rume7 <electengrhume@gmail.com>
2024-02-09 05:57:45 +0000

    Added link to support team

--- src/layouts/NavbarAndFooter/Footer.tsx
+++ src/layouts/NavbarAndFooter/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
         <div className="main-color">
             <footer className="container d-flex flex-wrap justify-content-between 
                 align-items-center py-5 main-color">
-                <p className="col-md-4 mb-0 text-white">&copy; Example Library App, Inc</p>
+                <p className="col-md-4 mb-0 text-white">&copy; Escrow Services App, Inc</p>
                 <ul className="nav navbar-dark col-md-4 justify-content-end">
                     <li className="nav-item">
                         <Link to="/home" className="nav-link px-2 text-white">
@@ -14,8 +14,8 @@ export const Footer = () => {
                         </Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/search" className="nav-link px-2 text-white">
-                            Search Books
+                        <Link to="/support" className="nav-link px-2 text-white">
+                            Support Team
                         </Link>
                     </li>
                 </ul>
